1fa634d7793dba8488029d0bc0dcf556eba183d7
Ynara Oliveira <ynaraoliveira.s@gmail.com>
2021-06-18 18:38:25 -0300

    Add files via upload

--- days/day1/main.py
+++ days/day1/main.py
@@ -1,1 +1,6 @@
-print("Hello " + input("What is your name?") + "!")
+print("Welcome to the band name generator.")
+
+city = input("Which city did you grow up in?\n").lower().capitalize()
+pet = input("What's the name of your first pet?\n").lower().capitalize()
+
+print("Your band name could be " + city + " " + pet)
