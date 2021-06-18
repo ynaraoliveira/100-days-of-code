fa43c5b6691a9c1c627f9def2f6a4423c1c89f53
Ynara Oliveira <ynaraoliveira.s@gmail.com>
2021-06-18 18:39:47 -0300

    Add files via upload

---
+++ days/day1/variables-exercise.py
@@ -0,0 +1,8 @@
+name = input("What is your name?")
+length = len(name)
+print(length)
+
+# python separate variable name with underscore
+user_name = "cathy3"
+
+# print( len( input("What is your name?")))
